d912ada5a7b5cdd11d45ab7832f90bb47a321097
NurikFun <nurik.iskakov.99@mail.ru>
2021-06-30 11:55:44 +0600

    Add project files.

---
+++ PatternSingleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var value = LazySingleton.Instance;
+
+            value.Calculate(5, 6);
+
+
+            Console.ReadKey();
+        }
+    }
+}
